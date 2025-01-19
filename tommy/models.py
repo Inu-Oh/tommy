7d6ccfbfb6356798387d6c5d8e5bbead50af8089
Inu Oh <ethanol-mace-swan@duck.com>
2025-01-18 19:11:34 -0500

    first commit

---
+++ tommy/models.py
@@ -0,0 +1,52 @@
+from django.core.validators import MinLengthValidator
+from django.db import models
+
+
+# Create your models here.
+class Word(models.Model):
+    word = models.CharField(
+        max_length=39,
+        validators=[MinLengthValidator(2, "A word must have at least one letter.")],
+    )
+    word_translation = models.ManyToManyField('self')
+
+    ENGLISH = 'EN'
+    FRENCH = 'FR'
+    CHINESE = 'CN'
+    LANGUAGE_CHOICES = [
+        (ENGLISH, 'English'),
+        (FRENCH, 'French'),
+        (CHINESE, 'Chinese'),
+    ]
+    language = models.CharField(
+        max_length=2,
+        choices=LANGUAGE_CHOICES,
+        default=ENGLISH,
+    )
+
+    def __str__(self):
+        return self.word
+
+class Phrase(models.Model):
+    phrase = models.CharField(
+        max_length=248,
+        validators=[MinLengthValidator(5, "A phrase must have at least two words")],
+    )
+    phrase_translation = models.ManyToManyField('self')
+
+    ENGLISH = 'EN'
+    FRENCH = 'FR'
+    CHINESE = 'CN'
+    LANGUAGE_CHOICES = [
+        (ENGLISH, 'English'),
+        (FRENCH, 'French'),
+        (CHINESE, 'Chinese'),
+    ]
+    language = models.CharField(
+        max_length=2,
+        choices=LANGUAGE_CHOICES,
+        default=ENGLISH,
+    )
+
+    def __str__(self):
+        return self.phrase
