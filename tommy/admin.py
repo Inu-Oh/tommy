81a15eca7f692af339e941307fb7535607ef6206
Inu Oh <ethanol-mace-swan@duck.com>
2025-01-20 20:25:59 -0500

    model changes

--- tommy/admin.py
+++ tommy/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from tommy.models import Word, Phrase
+from tommy.models import Word, WordTranslation, Phrase, PhraseTranslation
 
 # Register your models here.
 admin.site.register(Word)
+admin.site.register(WordTranslation)
 admin.site.register(Phrase)
+admin.site.register(PhraseTranslation)
--- tommy/models.py
+++ tommy/models.py
@@ -2,12 +2,35 @@ from django.core.validators import MinLengthValidator, MaxValueValidator, MinVal
 from django.db import models
 
 
+class WordTranslation(models.Model):
+    word_translation = models.CharField(
+        max_length=39,
+        validators=[MinLengthValidator(1, "A word must have at least one letter.")]
+    )
+    ENGLISH = 'EN'
+    FRENCH = 'FR'
+    CHINESE = 'CN'
+    LANGUAGE_CHOICES = [
+        (ENGLISH, 'English'),
+        (FRENCH, 'French'),
+        (CHINESE, 'Chinese'),
+    ]
+    language = models.CharField(
+        max_length=2,
+        choices=LANGUAGE_CHOICES,
+        default=ENGLISH,
+    )
+
+    def __str__(self):
+        return self.word_translation
+    
+
 class Word(models.Model):
     word = models.CharField(
         max_length=39,
-        validators=[MinLengthValidator(2, "A word must have at least one letter.")],
+        validators=[MinLengthValidator(1, "A word must have at least one letter.")],
     )
-    word_translation = models.ManyToManyField('self', blank=True)
+    word_translation = models.ManyToManyField(WordTranslation)
 
     ENGLISH = 'EN'
     FRENCH = 'FR'
@@ -32,12 +55,36 @@ class Word(models.Model):
     def __str__(self):
         return self.word
 
+
+class PhraseTranslation(models.Model):
+    phrase_translation = models.CharField(
+        max_length=248,
+        validators=[MinLengthValidator(5, "A phrase must have at least two words")],
+    )
+    ENGLISH = 'EN'
+    FRENCH = 'FR'
+    CHINESE = 'CN'
+    LANGUAGE_CHOICES = [
+        (ENGLISH, 'English'),
+        (FRENCH, 'French'),
+        (CHINESE, 'Chinese'),
+    ]
+    language = models.CharField(
+        max_length=2,
+        choices=LANGUAGE_CHOICES,
+        default=ENGLISH,
+    )
+
+    def __str__(self):
+        return self.phrase_translation
+
+
 class Phrase(models.Model):
     phrase = models.CharField(
         max_length=248,
         validators=[MinLengthValidator(5, "A phrase must have at least two words")],
     )
-    phrase_translation = models.ManyToManyField('self', blank=True)
+    phrase_translation = models.ManyToManyField(PhraseTranslation)
 
     ENGLISH = 'EN'
     FRENCH = 'FR'
