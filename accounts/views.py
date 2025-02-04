5e5dc3afc1ef2af868fdd5886208010e6702cd41
Inu Oh <ethanol-mace-swan@duck.com>
2025-02-03 21:26:35 -0500

    fixed registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,24 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 
 class RegisterView(CreateView):
-    form = UserCreationForm()
-    success_url = reverse_lazy('tommy:home')
     template_name = 'registration/register.html'
+
+    def get(self, request):
+        form = UserCreationForm()
+        
+        context = {'form': form}
+        return render(request, self.template_name, context)
+    
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if not form.is_valid():
+            context = {'form': form}
+            return render(request, self.template_name, context)
+        
+        form.save()
+        success_url = reverse_lazy('tommy:home')
+        return reverse_lazy(success_url)
