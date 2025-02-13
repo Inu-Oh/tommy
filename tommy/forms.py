443a964d690ab7b39d47d724a6bb37a312278188
Inu Oh <ethanol-mace-swan@duck.com>
2025-02-12 19:55:28 -0500

    bootstrap5 to profile form

--- tommy/forms.py
+++ tommy/forms.py
@@ -8,3 +8,8 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['name', 'learning']
+    
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        self.fields['name'].label = "Prenom | First name "
+        self.fields['learning'].label = "Formation d'anglais | Learning French "
